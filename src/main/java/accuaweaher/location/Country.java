191e076af24386c17af352078d5a30461a505cf2
Olga <olga252318@mail.ru>
2024-05-02 22:33:34 +0300

    DZ03

---
+++ src/main/java/accuaweaher/location/Country.java
@@ -0,0 +1,44 @@
+package accuaweaher.location;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Country {
+
+    @JsonProperty("ID")
+    private String id;
+    @JsonProperty("LocalizedName")
+    private String localizedName;
+    @JsonProperty("EnglishName")
+    private String englishName;
+
+    @JsonProperty("ID")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("ID")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("LocalizedName")
+    public String getLocalizedName() {
+        return localizedName;
+    }
+
+    @JsonProperty("LocalizedName")
+    public void setLocalizedName(String localizedName) {
+        this.localizedName = localizedName;
+    }
+
+    @JsonProperty("EnglishName")
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    @JsonProperty("EnglishName")
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+}
---
+++ src/test/java/accuaweather/GetWeatherOneDayTest.java
@@ -0,0 +1,28 @@
+package accuaweather;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import accuaweaher.weather.Weather;
+
+import static io.restassured.RestAssured.given;
+
+public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
+
+    @Test
+    void getWeatherOneDay_shouldReturn() {
+
+        Weather response = given()
+                .queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl()+"/forecasts/v1/daily/1day/294021")
+                .then()
+                .statusCode(200)
+                .time(Matchers.lessThan(2000l))
+                .extract()
+                .response()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(1,response.getDailyForecasts().size());
+    }
+}
